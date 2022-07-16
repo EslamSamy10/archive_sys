a49974ab3ab092994ed2f729e156b65a0b85a26a
Eslam <eslamsamy50003@gmail.com>
2022-07-16 20:06:22 +0200

    commm___

--- assets/js/scanner.js
+++ assets/js/scanner.js
@@ -1,13 +1,1 @@
 
-  function initNiceSrcoll(){
-    $('.nice-scroll').niceScroll(".nice-scroll-con",{
-      cursorcolor:'black',
-      cursorwidth:'5px',
-      autohidemode:'scroll',
-      cursorborder:'1px solid #3e444e'
-    });
-  }
-  function resizeNiceSrcoll(){
-    $(this).getNiceScroll().resize();
-  }
-
